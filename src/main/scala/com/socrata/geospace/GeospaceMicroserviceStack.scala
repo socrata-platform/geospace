ac5a3a0e404a85c31935f54096a528a6c67572d4
Evan Chan <velvia@gmail.com>
2014-05-27 11:39:21 -0700

    Initial Geospace Scalatra template (from scalatra giter8 template)

---
+++ src/main/scala/com/socrata/geospace/GeospaceMicroserviceStack.scala
@@ -0,0 +1,22 @@
+package com.socrata.geospace
+
+import org.scalatra._
+import scalate.ScalateSupport
+import org.fusesource.scalate.{ TemplateEngine, Binding }
+import org.fusesource.scalate.layout.DefaultLayoutStrategy
+import javax.servlet.http.HttpServletRequest
+import collection.mutable
+
+trait GeospaceMicroserviceStack extends ScalatraServlet with ScalateSupport {
+
+  // What to do in case a route is not found.  This is from the Scalatra template
+  notFound {
+    // remove content type in case it was set through an action
+    contentType = null
+    // Try to render a ScalateTemplate if no route matched
+    findTemplate(requestPath) map { path =>
+      contentType = "text/html"
+      layoutTemplate(path)
+    } orElse serveStaticResource() getOrElse resourceNotFound()
+  }
+}
