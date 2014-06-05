09393cd620f6e8d54ccfe6441c0a2c5dbf2157e4
Urmila Nadkarni <urmila.nadkarni@socrata.com>
2014-06-05 09:19:48 -0700

    Use Zookeeper to find Soda Fountain

--- src/main/scala/com/socrata/geospace/GeospaceServlet.scala
+++ src/main/scala/com/socrata/geospace/GeospaceServlet.scala
@@ -1,5 +1,6 @@
 package com.socrata.geospace
 
+import com.rojoma.simplearm.util._
 import com.socrata.http.common.AuxiliaryData
 import org.apache.curator.x.discovery.ServiceDiscovery
 
@@ -13,8 +14,20 @@ class GeospaceServlet(config: GeospaceConfig, discovery: ServiceDiscovery[Auxili
     </html>
   }
 
-  get("/config-test") {
-    config.sodaFountain.port + "\n"
+  get("/zookeeper-test") {
+    var msg = "haven't done anything yet"
+    for { sf <- managed(new SodaFountainClient(discovery, config.sodaFountain.serviceName, config.curator.connectTimeout)) } {
+      try {
+        sf.start
+        sf.requestBuilder match {
+          case Some(rb) => msg = "Connected to Soda Fountain!\n"
+          case _ => msg = "Couldn't get Soda Fountain instance\n"
+        }
+      } catch {
+        case e: Exception => msg = "Couldn't start Soda Fountain client :/\n" + e.getClass + "\n" + e.getMessage + "\n" + e.getStackTraceString + "\n"
+      }
+    }
+    msg
   }
 
 }
