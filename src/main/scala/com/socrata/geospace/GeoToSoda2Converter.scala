f19339f9dec9f5e6722b29f8a270ef721bbc8dfc
Evan Chan <velvia@gmail.com>
2014-06-13 17:05:56 -0700

    Add the feature ID as an extra column when ingesting Shapefiles

--- src/main/scala/com/socrata/geospace/GeoToSoda2Converter.scala
+++ src/main/scala/com/socrata/geospace/GeoToSoda2Converter.scala
@@ -12,6 +12,15 @@ import org.opengis.feature.`type`.PropertyDescriptor
  * Generates Soda2 requests from geo schemata and feature collections
  */
 object GeoToSoda2Converter {
+  // Note: is there a better way to come up with a column name for feature ID?
+  // This here is a hack.
+  val FeatureIdColName = "_feature_id"
+  val FeatureIdColumnDef = JObject(Map(
+      "field_name" -> JString(FeatureIdColName),
+      "datatype"   -> JString("text"),
+      "name"       -> JString(FeatureIdColName)
+    ))
+
   /**
    * Maps shapefile types to Soda2 types
    */
@@ -29,12 +38,12 @@ object GeoToSoda2Converter {
    * @return Soda2 create request body
    */
   def getCreateBody(resourceName: String, schema: Schema): JValue = {
-    val columnSchemata = schema.getDescriptors.asScala.map(columnToJObject(_))
+    val columnSchemata = Seq(FeatureIdColumnDef) ++ schema.getDescriptors.asScala.map(columnToJObject(_))
 
     JObject(Map(
       "resource_name" -> JString(resourceName),
       "name"          -> JString(resourceName),
-      "columns"       -> JArray(columnSchemata.toSeq)
+      "columns"       -> JArray(columnSchemata)
     ))
   }
 
@@ -83,6 +92,6 @@ object GeoToSoda2Converter {
       case (attr, name)        => name -> JString(attr.toString)
     }
 
-    JObject(fields.toMap)
+    JObject(fields.toMap + (FeatureIdColName -> JString(feature.getID)))
   }
 }
