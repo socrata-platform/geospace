b3d71c26bffbfe37c863855e9ca1a9b150201af3
Urmila Nadkarni <urmila.nadkarni@socrata.com>
2014-10-16 15:55:32 -0700

    Add comment to explain relaxed lon/lat validation values

--- src/main/scala/com/socrata/geospace/feature/FeatureValidator.scala
+++ src/main/scala/com/socrata/geospace/feature/FeatureValidator.scala
@@ -28,6 +28,8 @@ object FeatureValidator extends Logging {
   // -180 <= lon <= 180
   //  -90 <= lat <= 90
   // If we switch projections, this logic will need to be updated
+  // The values below are slightly relaxed since reprojection can result in rounding to values like 180.00000041
+  // We only care about precision to 6 decimal places so this should be acceptable.
   private def offTheMapPoints(mp: MultiPolygon): Array[Coordinate] = mp.getCoordinates.filter { coords =>
     coords.x >= 180.000001 || coords.x <= -180.000001 || coords.y >= 90.000001 || coords.y <= -90.000001
   }
