bbcad447663db75407b7119db4182c8bd8785f48
sokrahta <john.krah@socrata.com>
2014-12-01 15:53:28 -0800

    don't throw a new unknown exception

--- src/main/scala/com/socrata/geospace/regioncache/RegionCache.scala
+++ src/main/scala/com/socrata/geospace/regioncache/RegionCache.scala
@@ -109,8 +109,10 @@ abstract class RegionCache[T](maxEntries: Int = 100) //scalastyle:ignore
           payload.toOption.
             flatMap { jvalue => GeoJson.codec.decode(jvalue) }.
             collect { case FeatureCollectionJson(features, _) => getEntryFromFeatureJson(features, key.columnName) }.
-            getOrElse(throw new RuntimeException("Could not read GeoJSON from soda fountain: " + payload.get,
-            if (payload.isFailure) payload.failed.get else new Exception("unknown exception")))
+            getOrElse({
+              val errMsg = "Could not read GeoJSON from soda fountain: " + payload.get
+              if (payload.isFailure) { throw new RuntimeException(errMsg, payload.failed.get) }
+              else { throw new RuntimeException(errMsg) }})
         }
       }
     }
